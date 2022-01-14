9c7e7f2cdc79d2b9b6ebace5bc52d47076a09840
Edward <shamanspid1996@gmail.com>
2022-01-15 04:36:18 +0700

    create admin, config db

---
+++ admincp/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admincp</title>
+</head>
+
+<body>
+    <h3>Welcome to Admincp</h3>
+    <?php
+    include('modules/header.php');
+    include('modules/menu.php');
+    include('modules/main.php');
+    include('modules/footer.php');
+    ?>
+</body>
+
+</html>
